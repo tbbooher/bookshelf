8b72fa646acdd9f3553a3c0c22781218e6240a21
Tim Booher <tbbooher@users.noreply.github.com>
2016-07-05 23:24:10 -0400

    updated with holes!

--- DrawCircle.py
+++ DrawCircle.py
@@ -11,6 +11,8 @@ import adsk.core, adsk.fusion, traceback
 # - combine objects
 # - dogbone
 # - yeah
+cm = 2.54
+circle_radius = (1/8)*cm
 
 #// Create a transform to do move
 
@@ -35,7 +37,7 @@ def extrude_comp(profile, d, parent, name):
     extInput.setDistanceExtent(False, distance)
     extrude = extrudes.add(extInput)
     comp = extrude.parentComponent
-    if (name == 'shelves'):
+    if (name == 'shelves') and False: # not working yet
         move_component(comp, parent)
     comp.name = name
     
@@ -97,6 +99,17 @@ def draw_shelves(x,y,w,h,p, sketch, lines, n, parent):
         y_end   = y_start + p
         draw2(x+hp, y_start, x+w-hp, y_end, "shelf %d" % i, sketch, lines, parent)
         
+def draw_circles(start_x, end_x, y, n, h, p, num_circles, sketch):
+    # calculate number of shelves
+    a = (h - p*(2+n))/(n+1)
+    for i in range(1,n+1):
+        y_start = y + i*(p+a) 
+        y_end   = y_start + p
+        y0 = (y_start+y_end)/2
+        circles = sketch.sketchCurves.sketchCircles
+        for x in [start_x + float(x)/(num_circles-1)*(end_x-start_x) for x in range(num_circles)]:
+            circles.addByCenterRadius(adsk.core.Point3D.create(x, y0, 0), circle_radius)
+        
 def set_param(name, value, units, comment, design):
     value_input = adsk.core.ValueInput.createByReal(float(value))
     design.userParameters.add(name, value_input, units, comment)
@@ -120,10 +133,10 @@ def run(context):
         inputs = {}
         inputs['x'] = 0
         inputs['y'] = 0
-        inputs['w'] = 46.0*2.5
-        inputs['h'] = 8*12*2.5
-        inputs['p'] = 0.706*2.5
-        inputs['d'] = 10*2.5
+        inputs['w'] = 46.0*cm
+        inputs['h'] = 8*12*cm
+        inputs['p'] = 0.706*cm
+        inputs['d'] = 10*cm
         inputs['shelves'] = 8
                
         for key, value in inputs.items():
@@ -160,10 +173,18 @@ def run(context):
         extrude_all(sketch, p, comp)
                 
         shelves_comp = createNewComponent(comp)
-        shelves_comp.name = 'shelves'        
+        shelves_comp.name = 'shelves'      
         sketch, lines = create_sketch('shelves', xyPlane, sketches)
         draw_shelves(x,y,w,h,p, sketch, lines, shelves, shelves_comp)
         extrude_all(sketch, d - p/2, shelves_comp)
+        
+        circles_comp = createNewComponent(comp)
+        circles_comp.name = 'holes'
+        hole_sketch, lines = create_sketch('holes', xyPlane, sketches)
+        o = 2*cm
+        draw_circles(x+p+o, x+w-p-o, y, shelves, h, p, 4, hole_sketch)
+        
+        extrude_all(hole_sketch, p, circles_comp)
         
         if False:
             horz_tabs = createNewComponent(comp)
